d1ba95f79679199e3cfc3f501e340126958d1e4a
Vedang Jani <vedangjani027@gmail.com>
2024-08-26 18:54:12 +0530

    - Installed EF Core packages and setup the db context class.

--- DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationsWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationsWithEFCoreApp
 {
     public class Program
@@ -8,6 +11,9 @@ namespace DbOperationsWithEFCoreApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
